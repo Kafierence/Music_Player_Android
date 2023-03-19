8f0583ef712202f9cc3b6230fc9bd65f5ec293a8
Kafierence <vannam34760@gmail.com>
2023-03-19 23:11:35 +0700

    update library fragment

--- app/src/main/java/hcmute/edu/vn/music_player/Fragment/LibraryFragment.java
+++ app/src/main/java/hcmute/edu/vn/music_player/Fragment/LibraryFragment.java
@@ -1,5 +1,6 @@
 package hcmute.edu.vn.music_player.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
+import hcmute.edu.vn.music_player.Activity.MainActivity;
+import hcmute.edu.vn.music_player.Activity.SplashScreen;
 import hcmute.edu.vn.music_player.R;
+import hcmute.edu.vn.music_player.databinding.FragmentLibraryBinding;
 
 
 public class LibraryFragment extends Fragment {
-
+    FragmentLibraryBinding binding;
     public LibraryFragment() {
         // Required empty public constructor
     }
@@ -21,6 +26,9 @@ public class LibraryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+        binding= FragmentLibraryBinding.inflate(inflater,container,false);
+        View view=binding.getRoot();
+
+        return  view;
     }
 }
