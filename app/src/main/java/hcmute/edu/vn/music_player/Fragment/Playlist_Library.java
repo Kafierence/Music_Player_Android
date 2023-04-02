b4503f702d295d0e7d9e311c7aea1eabd816ea0a
Kafierence <vannam34760@gmail.com>
2023-04-02 21:41:07 +0700

    Fix Media Player

--- app/src/main/java/hcmute/edu/vn/music_player/Fragment/Playlist_Library.java
+++ app/src/main/java/hcmute/edu/vn/music_player/Fragment/Playlist_Library.java
@@ -1,5 +1,6 @@
 package hcmute.edu.vn.music_player.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import hcmute.edu.vn.music_player.Activity.PlayMedia;
 import hcmute.edu.vn.music_player.R;
 import hcmute.edu.vn.music_player.databinding.FragmentPlaylistLibraryBinding;
 
@@ -27,6 +29,8 @@ public class Playlist_Library extends Fragment {
                              Bundle savedInstanceState) {
       binding=FragmentPlaylistLibraryBinding.inflate(inflater,container,false);
         View view=binding.getRoot();
+        Intent intent = new Intent(getActivity(), PlayMedia.class);
+        startActivity(intent);
         return view;
     }
 }
