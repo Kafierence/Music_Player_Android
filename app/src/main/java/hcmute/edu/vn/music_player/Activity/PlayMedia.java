7f8b2ad4770d8b4886831b80183f1130efe341ed
Kafierence <vannam34760@gmail.com>
2023-04-15 22:25:21 +0700

    Fix Playlist Loading and Display Media

--- app/src/main/java/hcmute/edu/vn/music_player/Activity/PlayMedia.java
+++ app/src/main/java/hcmute/edu/vn/music_player/Activity/PlayMedia.java
@@ -1,6 +1,8 @@
 package hcmute.edu.vn.music_player.Activity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,39 +11,55 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import hcmute.edu.vn.music_player.Models.Track;
 import hcmute.edu.vn.music_player.R;
 import hcmute.edu.vn.music_player.databinding.ActivityPlayMediaBinding;
 
-public class PlayMedia extends AppCompatActivity implements View.OnClickListener{
+public class    PlayMedia extends AppCompatActivity implements View.OnClickListener{
     // views declartion
-    TextView tvTime, tvDuration;
+    TextView tvTime, tvDuration,songName,artistName;
     SeekBar seekBarTime, seekBarVolume;
     Button btnPlay;
 
     MediaPlayer musicPlayer;
-
+    Track trackSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_media);
 
+        //setting get data from intent
+        Intent intent = getIntent();
+        trackSong=(Track)intent.getSerializableExtra("track");
+
+
         // hide the actionbar
 
-
+        // get Data Texview
         tvTime = findViewById(R.id.tvTime);
         tvDuration = findViewById(R.id.tvDuration);
+        songName=findViewById(R.id.song_name);
+        artistName=findViewById(R.id.song_artist_name);
+
+        //
         seekBarTime = findViewById(R.id.seekBarTime);
         seekBarVolume = findViewById(R.id.seekBarVolume);
         btnPlay = findViewById(R.id.btnPlay);
 
-        musicPlayer = MediaPlayer.create(this, R.raw.m2);
+
+        //Load data music to
+        musicPlayer = MediaPlayer.create(this, Uri.parse(trackSong.getSource()));
+        songName.setText(trackSong.getName().toString());
+        artistName.setText(trackSong.getArtistName().toString());
         musicPlayer.setLooping(true);
         musicPlayer.seekTo(0);
         musicPlayer.setVolume(0.5f, 0.5f);
-
+        //pass data get from track
         String duration = millisecondsToString(musicPlayer.getDuration());
         tvDuration.setText(duration);
+
+
 
         btnPlay.setOnClickListener(this);
 
